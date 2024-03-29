273e9c5f7ca63233c984827aa89453e8aa41de90
Vinicius Gomes <vinicius.svgomes@gmail.com>
2024-03-29 13:18:02 -0300

    itr working

--- src/main.py
+++ src/main.py
@@ -1,71 +1,38 @@
 import sys
 import socket
 import struct
-from command import Command
-from errors import MissingArguments, UnknownCommand, WrongNumberOfArguments
 
 
-def valid_command_arguments(command: Command, arguments: list[str]):
-    """Validate if the received arguments match the command."""
+def make_sas(response: bytes):
+    _, net_id, nonce, token = struct.unpack("!h 12s i 64s", response)
+    net_id = net_id.decode().strip()
+    token = token.decode()
+    return f"{net_id}:{nonce}:{token}"
 
-    match command:
-        case Command.ITR:
-            return len(arguments) == 2
-        case Command.ITV:
-            return len(arguments) == 1
-        case Command.GTR:
-            return len(arguments) >= 2
-        case Command.GTV:
-            return len(arguments) == 1
+
+def make_gas():
+    pass
 
 
 def main():
-    """Main program function"""
-
-    if len(sys.argv) < 4:
-        raise MissingArguments(3, len(sys.argv) - 1)
-
     host, port, command = sys.argv[1:4]
-
-    try:
-        command = Command(command)
-    except Exception as exc:
-        raise UnknownCommand(command) from exc
-
+    server_address = (host, int(port))
     args = sys.argv[4:]
 
-    if not valid_command_arguments(command, args):
-        raise WrongNumberOfArguments(command)
-
-    # with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    #     s.connect((host, int(port)))
-    match command:
-        case Command.ITR:
-            net_id, nonce = args
-            print(net_id, nonce)
-            message = struct.pack('h12si', 1,
-                                  bytes(
-                                      net_id.ljust(12, " "),
-                                      encoding="ascii"
-                                  ),
-                                  int(nonce))
-            print("Message sent: ", message)
-            # s.send(message)
-            # response = s.recv(82)
-            # print("Response received: ", response)
-        case Command.ITV:
-            net_id, nonce, token = args[0].split(":")
-            print(net_id, nonce, token)
-        case Command.GTR:
-            n = args[0]
-            sas = args[1:]
-            print(n)
-            print(sas)
-        case Command.GTV:
-            splitted = args[0].split("+")
-            sas = splitted[:-1]
-            token = splitted[-1]
-            print(sas, token)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.connect(server_address)
+        match command:
+            case "itr":
+                net_id, nonce = args
+                message = struct.pack(
+                    "!h 12s i",
+                    1,
+                    net_id.ljust(12, " ").encode(),
+                    int(nonce),
+                )
+                sock.send(message)
+                response = sock.recv(82)
+                print(make_sas(response))
 
 
 if __name__ == "__main__":
