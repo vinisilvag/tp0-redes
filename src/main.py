2e82c74fbd64e3d8efd9edcfd74f8f7ecf884ee2
Vinicius Gomes <vinicius.svgomes@gmail.com>
2024-04-01 23:34:19 -0300

    Almost done

--- src/main.py
+++ src/main.py
@@ -1,113 +1,70 @@
+"""
+main.py
+
+Main module of the project. Serves as the entry point for the project and contains the main functionality.
+
+Author: Vinicius Gomes
+Date: 01/04/2024
+"""
+
 import sys
 import socket
-import struct
-
-
-def make_sas(response: bytes):
-    """
-    Receive a server response and unpack it into an SAS string
-    """
-
-    _, net_id, nonce, sas_token = struct.unpack("!h12si64s", response)
-    net_id = net_id.decode().strip()
-    sas_token = sas_token.decode()
-    return f"{net_id}:{nonce}:{sas_token}"
-
-
-def make_gas(n: int, response: bytes):
-    """
-    Receive a server response and unpack it into an GAS string
-    """
-
-    gas = []
-    _, _, * \
-        group_members_sas, gas_token = struct.unpack(
-            f"!hh{'80s' * int(n)}64s", response)
-    for sas in group_members_sas:
-        net_id, nonce, sas_token = struct.unpack("!12si64s", sas)
-        net_id = net_id.decode().strip()
-        sas_token = sas_token.decode()
-        gas.append(f"{net_id}:{nonce}:{sas_token}")
-    gas.append(gas_token.decode())
-    return "+".join(gas)
-
-
-def encode_multiple_sas(group_members_sas):
-    """
-    Receive an array of SAS strings and encode it all in a byte string
-    """
-
-    request = []
-    for sas in group_members_sas:
-        net_id, nonce, token = sas.split(":")
-        request.append(struct.pack("!12si64s", net_id.ljust(
-            12, " ").encode(), int(nonce), token.encode()))
-    return request
+from constants import COMMANDS
+from errors import (
+    WrongCommandNumberArguments,
+    MissingArguments,
+    UnknownCommand,
+    RequestFailed,
+)
+from utils import (
+    get_address_family,
+    valid_arg_count,
+    encode_message,
+    has_error,
+    decode_response,
+)
 
 
 def main():
+    """
+    Main function of the project. It is executed when the script is run.
+    """
+
+    if len(sys.argv) < 4:
+        raise MissingArguments(3, len(sys.argv) - 1)
+
     host, port, command = sys.argv[1:4]
     server_address = (host, int(port))
     args = sys.argv[4:]
 
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    if command not in COMMANDS:
+        raise UnknownCommand(command)
+
+    if not valid_arg_count(command, args):
+        raise WrongCommandNumberArguments(command)
+
+    with socket.socket(get_address_family(host, int(port)), socket.SOCK_DGRAM) as sock:
+        sock.settimeout(10)
         sock.connect(server_address)
-        match command:
-            case "itr":
-                net_id, nonce = args
-                message = struct.pack(
-                    "!h12si",
-                    1,
-                    net_id.ljust(12, " ").encode(),
-                    int(nonce),
-                )
+        succeeded = False
+
+        while not succeeded:
+            try:
+                message, n = encode_message(command, args)
                 sock.send(message)
-                response = sock.recv(82)
-                print(make_sas(response))
-            case "itv":
-                member_sas = args[0]
-                net_id, nonce, token = member_sas.split(":")
-                message = struct.pack(
-                    "!h12si64s",
-                    3,
-                    net_id.ljust(12, " ").encode(),
-                    int(nonce),
-                    token.encode()
-                )
-                sock.send(message)
-                response = sock.recv(83)
-                _, _, _, _, valid = struct.unpack("!h12si64sb", response)
-                print(valid)
-            case "gtr":
-                n = args[0]
-                group_members_sas = args[1:]
-                message = struct.pack(
-                    f"!hh{'80s' * int(n)}",
-                    5,
-                    int(n),
-                    *encode_multiple_sas(group_members_sas),
-                )
-                sock.send(message)
-                response = sock.recv(4 + (80 * int(n)) + 64)
-                print(make_gas(int(n), response))
-            case "gtv":
-                gas = args[0]
-                gas_token = gas.split("+")[-1]
-                group_members_sas = gas.split("+")[:-1]
-                n = len(group_members_sas)
-                message = struct.pack(
-                    f"!hh{'80s' * n}64s",
-                    7,
-                    n,
-                    *encode_multiple_sas(group_members_sas),
-                    gas_token.encode()
-                )
-                sock.send(message)
-                response = sock.recv(4 + (80 * n) + 64 + 1)
-                _, _, * \
-                    _, valid = struct.unpack(
-                        f"!hh{'80s' * n}64sb", response)
-                print(valid)
+                response = sock.recv(1024)
+
+                error, error_code = has_error(response)
+                if error:
+                    raise RequestFailed(error_code)
+
+                print(decode_response(command, response, n))
+
+                # if reached it means that the message was successfully sended
+                # and an expected response was obtained
+                succeeded = True
+            except socket.timeout:
+                print("Failed to sent the message. Resending.")
 
 
 if __name__ == "__main__":
---
+++ src/utils.py
@@ -0,0 +1,187 @@
+"""
+utils.py
+
+Module with utility functions.
+"""
+
+import struct
+import socket
+from errors import InvalidHost
+
+
+def has_connection_family_available(addr_family, available):
+    """Check if a certain connection type is available."""
+
+    if addr_family in [conn[0] for conn in available]:
+        return True
+
+    return False
+
+
+def get_address_family(host: str, port: int):
+    """Get the address family socket type for the given host and port."""
+
+    addr_info = socket.getaddrinfo(host, port, 0, socket.SOCK_DGRAM)
+    available = [(info[0], info[4]) for info in addr_info]
+
+    if len(available) == 0:
+        raise InvalidHost(host, port)
+
+    try:
+        socket.inet_pton(socket.AF_INET, host)
+        if has_connection_family_available(socket.AF_INET, available):
+            return socket.AF_INET
+    except socket.error:
+        pass
+
+    try:
+        socket.inet_pton(socket.AF_INET6, host)
+        if has_connection_family_available(socket.AF_INET6, available):
+            return socket.AF_INET6
+    except socket.error:
+        pass
+
+    try:
+        socket.gethostbyname(host)
+        # get the first address family available
+        return available[0][0]
+    except socket.gaierror:
+        pass
+
+    raise InvalidHost(host, port)
+
+
+def valid_arg_count(command: str, args: list[str]):
+    """Check if the number of arguments is valid for the given command."""
+
+    match command:
+        case "itr":
+            return len(args) == 2
+        case "itv":
+            return len(args) == 1
+        case "gtr":
+            return len(args) >= 2
+        case "gtv":
+            return len(args) == 1
+
+
+def make_sas(response: bytes):
+    """Receive a server response and unpack it into an SAS string."""
+
+    _, net_id, nonce, sas_token = struct.unpack("!h12si64s", response)
+    net_id = net_id.decode().strip()
+    sas_token = sas_token.decode()
+    return f"{net_id}:{nonce}:{sas_token}"
+
+
+def make_gas(n: int, response: bytes):
+    """Receive a server response and unpack it into an GAS string."""
+
+    gas = []
+    _, _, *group_members_sas, gas_token = struct.unpack(
+        f"!hh{'80s' * int(n)}64s", response
+    )
+    for sas in group_members_sas:
+        net_id, nonce, sas_token = struct.unpack("!12si64s", sas)
+        net_id = net_id.decode().strip()
+        sas_token = sas_token.decode()
+        gas.append(f"{net_id}:{nonce}:{sas_token}")
+    gas.append(gas_token.decode())
+    return "+".join(gas)
+
+
+def encode_multiple_sas(group_members_sas):
+    """Receive an array of SAS strings and encode it all in a byte string"""
+
+    request = []
+    for sas in group_members_sas:
+        net_id, nonce, token = sas.split(":")
+        request.append(
+            struct.pack(
+                "!12si64s", net_id.encode("ascii"), int(nonce), token.encode("ascii")
+            )
+        )
+    return request
+
+
+def encode_message(command: str, args: list[str]):
+    """Encode a message based on the given command and arguments."""
+
+    match command:
+        case "itr":
+            net_id, nonce = args
+            return (
+                struct.pack(
+                    "!h12si",
+                    1,
+                    net_id.encode("ascii"),
+                    int(nonce),
+                ),
+                None,
+            )
+        case "itv":
+            member_sas = args[0]
+            net_id, nonce, token = member_sas.split(":")
+            return (
+                struct.pack(
+                    "!h12si64s",
+                    3,
+                    net_id.encode("ascii"),
+                    int(nonce),
+                    token.encode("ascii"),
+                ),
+                None,
+            )
+        case "gtr":
+            n = int(args[0])
+            group_members_sas = args[1:]
+            return (
+                struct.pack(
+                    f"!hh{'80s' * n}",
+                    5,
+                    n,
+                    *encode_multiple_sas(group_members_sas),
+                ),
+                n,
+            )
+        case "gtv":
+            gas = args[0]
+            gas_token = gas.split("+")[-1]
+            group_members_sas = gas.split("+")[:-1]
+            n = len(group_members_sas)
+            return (
+                struct.pack(
+                    f"!hh{'80s' * n}64s",
+                    7,
+                    n,
+                    *encode_multiple_sas(group_members_sas),
+                    gas_token.encode("ascii"),
+                ),
+                n,
+            )
+
+
+def decode_response(command: str, response, n: int = None):
+    """Decode a response based on the given command and response data."""
+
+    match command:
+        case "itr":
+            return make_sas(response)
+        case "itv":
+            *_, valid = struct.unpack("!h12si64sb", response)
+            return valid
+        case "gtr":
+            return make_gas(n, response)
+        case "gtv":
+            *_, valid = struct.unpack(f"!hh{'80s' * n}64sb", response)
+            return valid
+
+
+def has_error(response):
+    """Check if a response contains an error code."""
+
+    try:
+        _, error_code = struct.unpack("!hh", response)
+        return True, error_code
+    except struct.error:
+        return False, None
